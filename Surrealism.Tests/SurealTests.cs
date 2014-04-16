eb306b9ae5c7f509b99598a0e959ced83b4ad261
Aaron Powell <me@aaron-powell.com>
2014-04-16 22:01:45 +0800

    initial revision

---
+++ Surrealism.Tests/SurealTests.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Surrealism.Tests
+{
+    [TestFixture]
+    class SurealTests
+    {
+        [Test]
+        public void DefaultConstructor_WillBeZero()
+        {
+            var s = new Surreal();
+
+            Assert.That(s.IsZero(), Is.EqualTo(true));
+        }
+
+        [TestCase(1)]
+        [TestCase(10)]
+        public void PositiveNumberToCtor_WillNotBeZero(int num)
+        {
+            var s = new Surreal(num);
+
+            Assert.That(s.IsZero(), Is.EqualTo(false));
+        }
+
+        [TestCase(-1)]
+        [TestCase(-10)]
+        public void NegativeNumberToCtor_WillNotBeZero(int num)
+        {
+            var s = new Surreal(num);
+
+            Assert.That(s.IsZero(), Is.EqualTo(false));
+        }
+
+        [TestCase(1)]
+        [TestCase(10)]
+        public void PositiveNumber_WillBePositive(int num)
+        {
+            var s = new Surreal(num);
+            Assert.That(s.IsPositive(), Is.EqualTo(true));
+        }
+
+        [TestCase(-1)]
+        [TestCase(-10)]
+        public void NegativeNumber_WillBePositive(int num)
+        {
+            var s = new Surreal(num);
+            Assert.That(s.IsNegative(), Is.EqualTo(true));
+        }
+
+        [TestCase(1, 1)]
+        [TestCase(10, 10)]
+        [TestCase(-1, -1)]
+        [TestCase(-10, -10)]
+        public void MatchingRealNumbers_ShouldMatchSurreal(int first, int second)
+        {
+            var s1 = new Surreal(first);
+            var s2 = new Surreal(second);
+
+            Assert.That(s1.IsEqualTo(s2), Is.EqualTo(true));
+        }
+
+        [TestCase(1, 2)]
+        [TestCase(2, 2)]
+        [TestCase(10, 20)]
+        [TestCase(-1, 1)]
+        [TestCase(-10, -1)]
+        public void FirstNumber_LessThanOrEqualTo_SecondNumber(int a, int b)
+        {
+            var s1 = new Surreal(a);
+            var s2 = new Surreal(b);
+
+            Assert.That(s1.IsLessThanOrEqualTo(s2), Is.EqualTo(true));
+        }
+
+        [TestCase(1, 2)]
+        [TestCase(10, 20)]
+        [TestCase(-1, 1)]
+        [TestCase(-10, -1)]
+        public void FirstNumber_LessThan_SecondNumber(int a, int b)
+        {
+            var s1 = new Surreal(a);
+            var s2 = new Surreal(b);
+
+            Assert.That(s1.IsLessThan(s2), Is.EqualTo(true));
+        }
+
+        [TestCase(2, 1)]
+        [TestCase(2, 2)]
+        [TestCase(20, 10)]
+        [TestCase(1, -1)]
+        [TestCase(-1, -10)]
+        public void FirstNumber_GreaterThanOrEqualTo_SecondNumber(int a, int b)
+        {
+            var s1 = new Surreal(a);
+            var s2 = new Surreal(b);
+
+            Assert.That(s1.IsGreaterThanOrEqualTo(s2), Is.EqualTo(true));
+        }
+
+        [TestCase(2, 1)]
+        [TestCase(20, 10)]
+        [TestCase(1, -1)]
+        [TestCase(-1, -10)]
+        public void FirstNumber_GreaterThan_SecondNumber(int a, int b)
+        {
+            var s1 = new Surreal(a);
+            var s2 = new Surreal(b);
+
+            Assert.That(s1.IsGreaterThan(s2), Is.EqualTo(true));
+        }
+    }
+}
---
+++ Surrealism/Surreal.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Surrealism
+{
+    public class Surreal
+    {
+        private static Func<dynamic> zero = () => new[] { new object[0], new object[0] };
+        private static Func<Surreal, dynamic> L = s => s.value.ElementAt(0);
+        private static Func<Surreal, dynamic> R = s => s.value.ElementAt(1);
+
+        private static int Size(dynamic s)
+        {
+            var i = 0;
+            while (s != null)
+            {
+                i++;
+                s = s.Length == 0 ? null : s[0];
+            }
+
+            return i;
+        }
+
+        internal IEnumerable<object> value;
+
+        public Surreal()
+        {
+            value = zero();
+        }
+
+        public Surreal(int num)
+            : this()
+        {
+            if (num == 0)
+                return;
+
+            if (num > 0)
+                for (int i = 0; i < num; i++)
+                    this.Increment();
+            else
+                for (int i = num; i < 0; i++)
+                    this.Decrement();
+        }
+
+        private Surreal(IEnumerable<object> surreal)
+        {
+            value = surreal;
+        }
+
+        private void Increment()
+        {
+            this.value = this.Successor().value;
+        }
+
+        private void Decrement()
+        {
+            this.value = this.Predecessor().value;
+        }
+
+        private Surreal Successor()
+        {
+            if (this.IsZero() || this.IsPositive())
+                return new Surreal(new[] { this.value, new object[0] });
+            return new Surreal(R(this));
+        }
+
+        private Surreal Predecessor()
+        {
+            if (this.IsZero() || this.IsNegative())
+                return new Surreal(new[] { new object[0], this.value });
+            return new Surreal(L(this));
+        }
+
+        public bool IsZero()
+        {
+            return L(this).Length == 0 && R(this).Length == 0;
+        }
+
+        public bool IsPositive()
+        {
+            return Size(L(this)) > Size(R(this));
+        }
+
+        public bool IsNegative()
+        {
+            return Size(L(this)) < Size(R(this));
+        }
+
+        public bool IsEqualTo(Surreal to)
+        {
+            return this.IsLessThanOrEqualTo(to) && this.IsGreaterThanOrEqualTo(to);
+        }
+
+        public bool IsLessThanOrEqualTo(Surreal to)
+        {
+            var me = new Surreal(this.value);
+            var them = new Surreal(to.value);
+
+            if ((me.IsNegative() || me.IsZero()) && (them.IsPositive() || them.IsZero()))
+                return true;
+
+            if ((me.IsPositive() || me.IsZero()) && (them.IsNegative() || them.IsZero()))
+                return false;
+
+            if (me.IsPositive() && them.IsPositive())
+            {
+                while (!me.IsZero() && !them.IsZero())
+                {
+                    me.Decrement();
+                    them.Decrement();
+                }
+                return me.IsZero();
+            }
+
+            if (me.IsNegative() && them.IsNegative())
+            {
+                while (!me.IsZero() && !them.IsZero())
+                {
+                    me.Increment();
+                    them.Increment();
+                }
+                return them.IsZero();
+            }
+
+            return false;
+        }
+
+        public bool IsGreaterThanOrEqualTo(Surreal to)
+        {
+            return to.IsLessThanOrEqualTo(this);
+        }
+
+        public bool IsLessThan(Surreal s2)
+        {
+            return !IsGreaterThanOrEqualTo(s2);
+        }
+
+        public bool IsGreaterThan(Surreal s2)
+        {
+            return !IsLessThanOrEqualTo(s2);
+        }
+    }
+}
