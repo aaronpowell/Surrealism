2251b701dfec168765bce0139d73f81c8438c15f
Aaron Powell <me@aaron-powell.com>
2017-01-04 11:03:01 +1100

    Moving tests to xunit and getting them running in VS

--- Surrealism.Tests/SurealTests.cs
+++ Surrealism.Tests/SurealTests.cs
@@ -1,137 +1,143 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using NUnit.Framework;
+using Xunit;
 
 namespace Surrealism.Tests
 {
-    [TestFixture]
-    class SurealTests
+    public class SurealTests
     {
-        [Test]
+        [Fact]
         public void DefaultConstructor_WillBeZero()
         {
             var s = new Surreal();
 
-            Assert.That(s.IsZero(), Is.EqualTo(true));
+            Assert.Equal(s.IsZero(), true);
         }
 
-        [TestCase(1)]
-        [TestCase(10)]
+        [Theory]
+        [InlineData(1)]
+        [InlineData(10)]
         public void PositiveNumberToCtor_WillNotBeZero(int num)
         {
             var s = new Surreal(num);
 
-            Assert.That(s.IsZero(), Is.EqualTo(false));
+            Assert.Equal(s.IsZero(), false);
         }
 
-        [TestCase(-1)]
-        [TestCase(-10)]
+        [Theory]
+        [InlineData(-1)]
+        [InlineData(-10)]
         public void NegativeNumberToCtor_WillNotBeZero(int num)
         {
             var s = new Surreal(num);
 
-            Assert.That(s.IsZero(), Is.EqualTo(false));
+            Assert.Equal(s.IsZero(), false);
         }
 
-        [TestCase(1)]
-        [TestCase(10)]
+        [Theory]
+        [InlineData(1)]
+        [InlineData(10)]
         public void PositiveNumber_WillBePositive(int num)
         {
             var s = new Surreal(num);
-            Assert.That(s.IsPositive(), Is.EqualTo(true));
+            Assert.Equal(s.IsPositive(), true);
         }
 
-        [TestCase(-1)]
-        [TestCase(-10)]
+        [Theory]
+        [InlineData(-1)]
+        [InlineData(-10)]
         public void NegativeNumber_WillBePositive(int num)
         {
             var s = new Surreal(num);
-            Assert.That(s.IsNegative(), Is.EqualTo(true));
+            Assert.Equal(s.IsNegative(), true);
         }
 
-        [TestCase(1, 1)]
-        [TestCase(10, 10)]
-        [TestCase(-1, -1)]
-        [TestCase(-10, -10)]
+        [Theory]
+        [InlineData(1, 1)]
+        [InlineData(10, 10)]
+        [InlineData(-1, -1)]
+        [InlineData(-10, -10)]
         public void MatchingRealNumbers_ShouldMatchSurreal(int first, int second)
         {
             var s1 = new Surreal(first);
             var s2 = new Surreal(second);
 
-            Assert.That(s1.IsEqualTo(s2), Is.EqualTo(true));
+            Assert.Equal(s1.IsEqualTo(s2), true);
         }
 
-        [TestCase(1, 2)]
-        [TestCase(2, 2)]
-        [TestCase(10, 20)]
-        [TestCase(-1, 1)]
-        [TestCase(-10, -1)]
+        [Theory]
+        [InlineData(1, 2)]
+        [InlineData(2, 2)]
+        [InlineData(10, 20)]
+        [InlineData(-1, 1)]
+        [InlineData(-10, -1)]
         public void FirstNumber_LessThanOrEqualTo_SecondNumber(int a, int b)
         {
             var s1 = new Surreal(a);
             var s2 = new Surreal(b);
 
-            Assert.That(s1.IsLessThanOrEqualTo(s2), Is.EqualTo(true));
+            Assert.Equal(s1.IsLessThanOrEqualTo(s2), true);
         }
 
-        [TestCase(1, 2)]
-        [TestCase(10, 20)]
-        [TestCase(-1, 1)]
-        [TestCase(-10, -1)]
+        [Theory]
+        [InlineData(1, 2)]
+        [InlineData(10, 20)]
+        [InlineData(-1, 1)]
+        [InlineData(-10, -1)]
         public void FirstNumber_LessThan_SecondNumber(int a, int b)
         {
             var s1 = new Surreal(a);
             var s2 = new Surreal(b);
 
-            Assert.That(s1.IsLessThan(s2), Is.EqualTo(true));
+            Assert.Equal(s1.IsLessThan(s2), true);
         }
 
-        [TestCase(2, 1)]
-        [TestCase(2, 2)]
-        [TestCase(20, 10)]
-        [TestCase(1, -1)]
-        [TestCase(-1, -10)]
+        [Theory]
+        [InlineData(2, 1)]
+        [InlineData(2, 2)]
+        [InlineData(20, 10)]
+        [InlineData(1, -1)]
+        [InlineData(-1, -10)]
         public void FirstNumber_GreaterThanOrEqualTo_SecondNumber(int a, int b)
         {
             var s1 = new Surreal(a);
             var s2 = new Surreal(b);
 
-            Assert.That(s1.IsGreaterThanOrEqualTo(s2), Is.EqualTo(true));
+            Assert.Equal(s1.IsGreaterThanOrEqualTo(s2), true);
         }
 
-        [TestCase(2, 1)]
-        [TestCase(20, 10)]
-        [TestCase(1, -1)]
-        [TestCase(-1, -10)]
+        [Theory]
+        [InlineData(2, 1)]
+        [InlineData(20, 10)]
+        [InlineData(1, -1)]
+        [InlineData(-1, -10)]
         public void FirstNumber_GreaterThan_SecondNumber(int a, int b)
         {
             var s1 = new Surreal(a);
             var s2 = new Surreal(b);
 
-            Assert.That(s1.IsGreaterThan(s2), Is.EqualTo(true));
+            Assert.Equal(s1.IsGreaterThan(s2), true);
         }
 
-        [TestCase(0)]
-        [TestCase(1)]
-        [TestCase(10)]
-        [TestCase(-1)]
-        [TestCase(-10)]
+        [Theory]
+        [InlineData(0)]
+        [InlineData(1)]
+        [InlineData(10)]
+        [InlineData(-1)]
+        [InlineData(-10)]
         public void SurrealNumber_CanBeConvertedToRealNumber(int num)
         {
             var s = new Surreal(num);
 
-            Assert.That(s.Real(), Is.EqualTo(num));
+            Assert.Equal(s.Real(), num);
         }
 
-        [TestCase(0, 1)]
-        [TestCase(1, 10)]
-        [TestCase(0, -1)]
-        [TestCase(-10, -1)]
-        [TestCase(1, -1)]
-        [TestCase(0, -10)]
+        [Theory]
+        [InlineData(0, 1)]
+        [InlineData(1, 10)]
+        [InlineData(0, -1)]
+        [InlineData(-10, -1)]
+        [InlineData(1, -1)]
+        [InlineData(0, -10)]
         public void CanAddTwoNumbers(int a, int b)
         {
             var s1 = new Surreal(a);
@@ -139,15 +145,16 @@ namespace Surrealism.Tests
 
             var result = s1 + s2;
 
-            Assert.That(result.Real(), Is.EqualTo(a + b));
+            Assert.Equal(result.Real(), a + b);
         }
 
-        [TestCase(0, 1)]
-        [TestCase(1, 10)]
-        [TestCase(0, -1)]
-        [TestCase(-10, -1)]
-        [TestCase(1, -1)]
-        [TestCase(0, -10)]
+        [Theory]
+        [InlineData(0, 1)]
+        [InlineData(1, 10)]
+        [InlineData(0, -1)]
+        [InlineData(-10, -1)]
+        [InlineData(1, -1)]
+        [InlineData(0, -10)]
         public void CanSubtractTwoNumbers(int a, int b)
         {
             var s1 = new Surreal(a);
@@ -155,13 +162,14 @@ namespace Surrealism.Tests
 
             var result = s1 - s2;
 
-            Assert.That(result.Real(), Is.EqualTo(a - b));
+            Assert.Equal(result.Real(), a - b);
         }
 
-        [TestCase(2, 10)]
-        [TestCase(0, -1)]
-        [TestCase(-10, -1)]
-        [TestCase(1, -1)]
+        [Theory]
+        [InlineData(2, 10)]
+        [InlineData(0, -1)]
+        [InlineData(-10, -1)]
+        [InlineData(1, -1)]
         public void CanMultiplyTwoNumbers(int a, int b)
         {
             var s1 = new Surreal(a);
@@ -169,13 +177,14 @@ namespace Surrealism.Tests
 
             var result = s1 * s2;
 
-            Assert.That(result.Real(), Is.EqualTo(a * b));
+            Assert.Equal(result.Real(), a * b);
         }
 
-        [TestCase(10, 2)]
-        [TestCase(9, 3)]
-        [TestCase(9, -3)]
-        [TestCase(-9, -3)]
+        [Theory]
+        [InlineData(10, 2)]
+        [InlineData(9, 3)]
+        [InlineData(9, -3)]
+        [InlineData(-9, -3)]
         public void CanDivideTwoNumbers(int a, int b)
         {
             var s1 = new Surreal(a);
@@ -183,23 +192,23 @@ namespace Surrealism.Tests
 
             var result = s1 / s2;
 
-            Assert.That(result.Real(), Is.EqualTo(a / b));
+            Assert.Equal(result.Real(), a / b);
         }
 
-        [Test]
+        [Fact]
         public void ZeroDenominator_WillThrowException()
         {
             var numerator = new Surreal(42);
             var denominator = new Surreal();
 
-            Assert.IsTrue(denominator.IsZero());
+            Assert.True(denominator.IsZero());
             Assert.Throws<DivideByZeroException>(() =>
             {
                 var x = numerator / denominator;
             });
         }
 
-        [Test]
+        [Fact]
         public void ZeroNumerator_WillReturnZero()
         {
             var numerator = new Surreal();
@@ -208,12 +217,13 @@ namespace Surrealism.Tests
 
             var result = numerator / denominator;
 
-            Assert.IsTrue(numerator.IsZero());
-            Assert.IsTrue(result.IsZero(), "0 / 42 should be zero");
+            Assert.True(numerator.IsZero());
+            Assert.True(result.IsZero(), "0 / 42 should be zero");
         }
 
-        [TestCase(1)]
-        [TestCase(-1)]
+        [Theory]
+        [InlineData(1)]
+        [InlineData(-1)]
         public void IncrementOperator_WillIncrementTheRealValue(int a)
         {
             var s = new Surreal(a);
@@ -221,11 +231,12 @@ namespace Surrealism.Tests
             var result = s++;
             var expected = a++;
 
-            Assert.That(result.Real(), Is.EqualTo(expected));
+            Assert.Equal(result.Real(), expected);
         }
 
-        [TestCase(1)]
-        [TestCase(-1)]
+        [Theory]
+        [InlineData(1)]
+        [InlineData(-1)]
         public void DecrementOperator_WillIncrementTheRealValue(int a)
         {
             var s = new Surreal(a);
@@ -233,7 +244,7 @@ namespace Surrealism.Tests
             var result = s--;
             var expected = a--;
 
-            Assert.That(result.Real(), Is.EqualTo(expected));
+            Assert.Equal(result.Real(), expected);
         }
     }
 }
